28e2d42684496c895ae8ec7036998444490a68d0
Kiriesshka <kirieshkaabuser@gmail.com>
2026-05-20 00:09:02 +0300

    багфикс в LineCollection, расширение функционала и добавлен TextMeshPro

--- subproject/Roguelike/Assets/Scripts/UI/LineCollection.cs
+++ subproject/Roguelike/Assets/Scripts/UI/LineCollection.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Roguelike.UI
@@ -7,15 +8,22 @@ namespace Roguelike.UI
     public class LineCollection : MonoBehaviour
     {
         [SerializeField] private List<GameObject> variants;
+        [SerializeField] private TMP_Text description;
         public List<string> values;
+        public List<string> descriptions;
         public int choosedId { get; private set; }
         public string choosedValue { get; private set; }
 
         public void SetControlled(int id)
         {
-            variants[id].transform.GetChild(0).gameObject.SetActive(true);
+            for (int i = 0; i < variants.Count; ++i)
+            {
+                variants[i].transform.GetChild(0).gameObject.SetActive(i == id);
+            }
             choosedId = id;
+            description.text = descriptions[choosedId];
             choosedValue = values[choosedId];
+            description.gameObject.SetActive(true);
         }
     }
 }
